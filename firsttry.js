b1f4a7eec7b820af41f43b318b780763d1f632e7
Andromeda <b13902064@csie.ntu.edu.tw>
2025-04-26 16:21:00 +0800

    first p5

---
+++ firsttry.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255, 220, 250); // 粉粉嫩嫩的背景
+  fill(255, 100, 180);
+  ellipse(200, 200, 150, 150); // 中間一個大粉圓～
+}
